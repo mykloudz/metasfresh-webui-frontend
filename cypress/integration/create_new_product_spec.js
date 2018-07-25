bda1cc2f19f6e173f08fc6ac77538538c29cfc25
metas-dh <daniela.hoffmann@metasfresh.com>
2018-07-25 16:39:47 +0200

    update

---
+++ cypress/integration/create_new_product_spec.js
@@ -0,0 +1,15 @@
+describe('Create New Product', function() {
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
+
+  context('Tabs', function() {
+    beforeEach(function() {
+      cy.visit('/window/140/2005598');
+      cy
+        .get('.header-breadcrumb-sitename')
+        .should('contain', '"<1000001>"');
+    });
+  });
+})
