165ee839d0d1e2c2501e30a5bc97dc5a4ff3b62c
metas-dh <daniela.hoffmann@metasfresh.com>
2018-07-26 10:56:12 +0200

    00

---
+++ cypress/integration/business_partner_from_setup_wizard_check_spec.js
@@ -0,0 +1,35 @@
+describe('Business partner window widgets test', function() {
+  before(function() {
+    // login before each test
+    cy.loginByForm();
+  });
+
+  context('Tabs', function() {
+    beforeEach(function() {
+      cy.visit('/window/123/2155894');
+      cy
+        .get('.header-breadcrumb-sitename')  // note: probably needs to be changed
+        .should('contain', 'testfirmaWebUI');
+    });
+
+    context('Vendor', function() {
+      it('Check if list widget works properly', function() {
+        cy
+          .get('.header-breadcrumb-sitename')
+          .should('contain', 'testfirmaWebUI');
+
+        cy
+          .get('Search Key')
+          .should('contain', 'testfirmaWebUI');
+
+        cy
+          .get('Name')
+          .should('contain', 'testfirmaWebUI');
+
+        cy
+          .get('Organisation')
+          .should('contain', 'testfirmaWebUI');
+      });
+    });
+  });
+});
