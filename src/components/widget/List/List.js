1336b82644bcfce7520a13e9710edbf2edfeac46
metas-mk <mark.krake@metasfresh.com>
2019-05-26 17:39:30 +0200

    lint fix

--- src/components/widget/List/List.js
+++ src/components/widget/List/List.js
@@ -365,9 +365,6 @@ const mapStateToProps = state => ({
   filter: state.windowHandler.filter,
 });
 
-export default connect(
-  mapStateToProps,
-  false,
-  false,
-  { withRef: true }
-)(ListWidget);
+export default connect(mapStateToProps, false, false, { withRef: true })(
+  ListWidget
+);
